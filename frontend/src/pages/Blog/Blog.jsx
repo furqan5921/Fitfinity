0d71ab2a855ff4deba8a59d1c2078be14a44f31c
JYOTIPM1999 <jyotipm1999@gmail.com>
2022-12-13 12:46:44 +0530

    Blog page started

--- frontend/src/pages/Blog/Blog.jsx
+++ frontend/src/pages/Blog/Blog.jsx
@@ -10,33 +10,34 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import React from "react";
+import styles from "./Blog.module.css";
 
-const recentPost = [
-  {
-    image:
-      "https://cronometer.com/blog/wp-content/uploads/2022/11/Blog-Img-DB.png",
-    text: "Mastering Diabetes with Cronometer",
-    date: "December 1, 2022",
-  },
-  {
-    image:
-      "https://cronometer.com/blog/wp-content/uploads/2022/09/cronometer-new-logo@2x.png",
-    text: "Our New Look: The Details Behind Cronometer’s Makeover",
-    date: "November 14, 2022",
-  },
-  {
-    image:
-      "https://cronometer.com/blog/wp-content/uploads/2022/06/Blog-Img-New-Improved.png",
-    text: "New & Improved: The Latest Cronometer Updates",
-    date: "November 14, 2022",
-  },
-  {
-    image:
-      "https://cronometer.com/blog/wp-content/uploads/2022/10/Blog-Img-Gummy-Bears.png",
-    text: "A Look Into Sugar-Free Sensitivities",
-    date: "October 27, 2022",
-  },
-];
+// const recentPost = [
+//   {
+//     image:
+//       "https://cronometer.com/blog/wp-content/uploads/2022/11/Blog-Img-DB.png",
+//     text: "Mastering Diabetes with Cronometer",
+//     date: "December 1, 2022",
+//   },
+//   {
+//     image:
+//       "https://cronometer.com/blog/wp-content/uploads/2022/09/cronometer-new-logo@2x.png",
+//     text: "Our New Look: The Details Behind Cronometer’s Makeover",
+//     date: "November 14, 2022",
+//   },
+//   {
+//     image:
+//       "https://cronometer.com/blog/wp-content/uploads/2022/06/Blog-Img-New-Improved.png",
+//     text: "New & Improved: The Latest Cronometer Updates",
+//     date: "November 14, 2022",
+//   },
+//   {
+//     image:
+//       "https://cronometer.com/blog/wp-content/uploads/2022/10/Blog-Img-Gummy-Bears.png",
+//     text: "A Look Into Sugar-Free Sensitivities",
+//     date: "October 27, 2022",
+//   },
+// ];
 
 const Blog = () => {
   return (
@@ -69,23 +70,61 @@ const Blog = () => {
             ></video>
           </Box>
         </Flex>
-        <Text as="b">RECENT POSTS</Text>
+        <Text as="b" w="75%" m={"auto"}>
+          RECENT POSTS
+        </Text>
         <Grid w={"75%"} m="auto" templateColumns="repeat(4, 1fr)" gap={6}>
-          {recentPost.map((el, i) => {
-            <GridItem
-              key={i}
-              w="100%"
-              p={"15px"}
+          <GridItem className={styles.gridItems}>
+            <Image
+              h={"150px"}
+              w={"100%"}
               borderRadius={"10px"}
-              boxShadow="rgba(99, 99, 99, 0.2) 0px 2px 8px 0px"
-            >
-              <Image borderRadius={"10px"} src={el.image} />
-              <Text fontSize={"2xl"} fontWeight={"extrabold"}>
-                {el.text}
-              </Text>
-              <Text>{el.date}</Text>
-            </GridItem>;
-          })}
+              src="https://cronometer.com/blog/wp-content/uploads/2022/11/Blog-Img-DB.png"
+            />
+            <Text fontSize={"2xl"} fontWeight={"extrabold"}>
+              Mastering Diabetes with Cronometer
+            </Text>
+            <Text>December 1, 2022</Text>
+          </GridItem>
+
+          <GridItem className={styles.gridItems}>
+            <Image
+              h={"150px"}
+              w={"100%"}
+              borderRadius={"10px"}
+              src="https://cronometer.com/blog/wp-content/uploads/2022/09/cronometer-new-logo@2x.png"
+            />
+            <Text fontSize={"2xl"} fontWeight={"extrabold"}>
+              Our New Look: The Details Behind Cronometer’s Makeover
+            </Text>
+            <Text>November 14, 2022</Text>
+          </GridItem>
+
+          <GridItem className={styles.gridItems}>
+            <Image
+              h={"150px"}
+              w={"100%"}
+              borderRadius={"10px"}
+              src="https://cronometer.com/blog/wp-content/uploads/2022/06/Blog-Img-New-Improved.png"
+            />
+            <Text fontSize={"2xl"} fontWeight={"extrabold"}>
+              New & Improved: The Latest Cronometer Updates
+            </Text>
+            <Text>November 14, 2022</Text>
+          </GridItem>
+
+          <GridItem className={styles.gridItems}>
+            <Image
+              h={"150px"}
+              w={"100%"}
+              borderRadius={"10px"}
+              src="https://cronometer.com/blog/wp-content/uploads/2022/10/Blog-Img-Gummy-Bears.png"
+            />
+            <Text fontSize={"2xl"} fontWeight={"extrabold"}>
+              A Look Into Sugar-Free Sensitivities
+            </Text>
+            <Text>October 27, 2022</Text>
+          </GridItem>
         </Grid>
       </Box>
     </>
@@ -94,24 +133,26 @@ const Blog = () => {
 
 export default Blog;
 
-// <GridItem w="100%">
-//             <Image src="https://cronometer.com/blog/wp-content/uploads/2022/09/cronometer-new-logo@2x.png" />
-//             <Text fontSize={"2xl"} fontWeight={"extrabold"}>
-//               Our New Look: The Details Behind Cronometer’s Makeover
-//             </Text>
-//             <Text>November 14, 2022</Text>
-//           </GridItem>
-//           <GridItem w="100%">
-//             <Image src="https://cronometer.com/blog/wp-content/uploads/2022/06/Blog-Img-New-Improved.png" />
-//             <Text fontSize={"2xl"} fontWeight={"extrabold"}>
-//               New & Improved: The Latest Cronometer Updates
-//             </Text>
-//             <Text>November 14, 2022</Text>
-//           </GridItem>
-//           <GridItem w="100%">
-//             <Image src="https://cronometer.com/blog/wp-content/uploads/2022/10/Blog-Img-Gummy-Bears.png" />
-//             <Text fontSize={"2xl"} fontWeight={"extrabold"}>
-//               A Look Into Sugar-Free Sensitivities
-//             </Text>
-//             <Text>October 27, 2022</Text>
-//           </GridItem>
+{
+  /* <GridItem w="100%">
+            <Image src="https://cronometer.com/blog/wp-content/uploads/2022/09/cronometer-new-logo@2x.png" />
+            <Text fontSize={"2xl"} fontWeight={"extrabold"}>
+              Our New Look: The Details Behind Cronometer’s Makeover
+            </Text>
+            <Text>November 14, 2022</Text>
+          </GridItem>
+          <GridItem w="100%">
+            <Image src="https://cronometer.com/blog/wp-content/uploads/2022/06/Blog-Img-New-Improved.png" />
+            <Text fontSize={"2xl"} fontWeight={"extrabold"}>
+              New & Improved: The Latest Cronometer Updates
+            </Text>
+            <Text>November 14, 2022</Text>
+          </GridItem>
+          <GridItem w="100%">
+            <Image src="https://cronometer.com/blog/wp-content/uploads/2022/10/Blog-Img-Gummy-Bears.png" />
+            <Text fontSize={"2xl"} fontWeight={"extrabold"}>
+              A Look Into Sugar-Free Sensitivities
+            </Text>
+            <Text>October 27, 2022</Text>
+          </GridItem> */
+}
